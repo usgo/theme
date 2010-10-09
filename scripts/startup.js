f9b5e3b901e7480beb3084d1e2d825e0de4c78b8
Joshua Simmons <joshua.simmons@emptypath.com>
2010-10-09 16:34:16 -0400

    move startup scripts to their own file

---
+++ scripts/startup.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    if (init_expanding_menu()) {
+        $('.mainnav').removeClass('flyout')
+                     .addClass('expand');
+        /* ie7fix only needed for flyout, so removeClass if javascript works */
+        $('.ie7fix').removeClass('ie7fix');
+    }
+    /* backups for browsers that don't support those css3 selectors yet */
+    $('tr:nth-child(odd)').addClass('odd');
+    $('tr:nth-child(even)').addClass('even');
+});
