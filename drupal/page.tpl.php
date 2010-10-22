c230e0a121744538017d0d3ad013e16270e22ef8
Joshua Simmons <joshua.simmons@emptypath.com>
2010-10-21 20:13:25 -0400

    fix typo ?> instead of >

--- drupal/page.tpl.php
+++ drupal/page.tpl.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <?php include(path_to_theme() . "/includes/meta.html"); ?>
         <title><?php print $head_title ?></title>
