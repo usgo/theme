5204f7dc8122c3a99239d09f8c45190639049071
Joshua Simmons <joshua.simmons@emptypath.com>
2011-09-02 20:34:06 -0400

    add IE fixes CSS files

--- drupal/template.php
+++ drupal/template.php
@@ -3,5 +3,11 @@ function kabocha_preprocess_page(&$variables) {
   if(!empty($variables['page']['sidebar_second'])) {
     drupal_add_css(path_to_theme() . '/style/right_col.css');
   }
+  drupal_add_css(path_to_theme() . '/style/ie6_fixes.css',
+                 array('browsers' => array('IE' => 'IE 6', '!IE' => FALSE)));
+  drupal_add_css(path_to_theme() . '/style/ie7_fixes.css',
+                 array('browsers' => array('IE' => 'IE 7', '!IE' => FALSE)));
+  drupal_add_css(path_to_theme() . '/style/ie_fixes.css',
+                 array('browsers' => array('IE' => 'lte IE 8', '!IE' => FALSE)));
 }
 ?>
