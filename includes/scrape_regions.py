137beb3f1822728674a52494146351d680e9ad6e
Michael Hiiva <michaelhiiva@gmail.com>
2019-01-14 21:17:44 +0000

    Added verbose to scrape_regions.py. Provided configuration file, so that the script can be ported to other systems.

--- includes/scrape_regions.py
+++ includes/scrape_regions.py
@@ -1,10 +1,45 @@
 from BeautifulSoup import BeautifulSoup as BS
+from pathlib import Path
 import urllib
+import os
+import sys
 
-url = "http://www.usgo.org/user/"
-write_dir = '/var/www/usgo.org/theme/includes'
+# Description:
+# scrape_regions.py scrapes the navwrap, navcol, extracol, pagefooter
+# regions of the usgo.org Drupal user page https://www.usgo.org/user or
+# the development site https://dev.usgo.org/user.
+#
+# Python Requirements:
+# - beatifulsoup
+# - pathlib
+#
+# Requirements:
+# - configured
+#   - usgo_drupal_scrape_conf.py in the current directory.
+# - AGA_URL
+#   - The environment which this script is running.
+# - AGA_WRITE_DIR
+#   - The directory to which the cached files will be written.
+
+# Check that the config file exists.
+usgo_drupal_scrape_config_file = Path("usgo_drupal_scrape_conf.py")
+
+if (not usgo_drupal_scrape_config_file.is_file()):
+     print("No usgo_drupal_scrape_conf.py provided.")
+     sys.exit()
+else:
+     import usgo_drupal_scrape_conf
+
+# URL to scrape.
+url = usgo_drupal_scrape_conf.AGA_URL
+
+# Directory to write out our cached files.
+write_dir = usgo_drupal_scrape_conf.AGA_WRITE_DIR
+
+# Page object of the scraped url.
 page = urllib.urlopen(url).read()
 
+# Define the regions to be scraped in a list
 div_ids = ['navwrap', 'navcol', 'extracol', 'postfooter']
 
 soup = BS(page)
@@ -12,6 +47,7 @@ soup = BS(page)
 if write_dir == '':
     write_dir = '.'
 
+# Write out the each region to a [region].cached.html file.
 for id in div_ids:
     tree = soup.find('div', id = id)
     if tree != None:
