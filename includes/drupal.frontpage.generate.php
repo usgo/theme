017c19af55520fed226da658c70445fcad601f61
Michael Hiiva <michaelhiiva@gmail.com>
2019-02-14 22:04:55 +0000

    Added dynamic way for drupal.frontpage.generate.php to add /news directory.

--- includes/drupal.frontpage.generate.php
+++ includes/drupal.frontpage.generate.php
@@ -1,5 +1,11 @@
-<?php require('/var/www/usgo.org/news/wp-blog-header.php');
+<?php
+// Get the location of the /news directory.
+$drupal_frontpage_news_dir = dirname(__DIR__, 2) . "/news";
+
+require($drupal_frontpage_news_dir . '/wp-blog-header.php');
+
 query_posts('cat=712,-1182&showposts=5');
+
 if (!have_posts())
 {
     query_posts('cat=-1182&showposts=3');
