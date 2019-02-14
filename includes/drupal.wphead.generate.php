c91d4930e67721103c17c747904ddadf85b8e998
Michael Hiiva <michaelhiiva@gmail.com>
2019-02-14 21:44:55 +0000

    Made the require of wp-blog-header.php dynamic.

--- includes/drupal.wphead.generate.php
+++ includes/drupal.wphead.generate.php
@@ -1,4 +1,8 @@
-<?php require('/var/www/usgo.org/news/wp-blog-header.php'); ?>
+<?php
+$wphead_news_directory = dirname(__DIR__, 2) . "/news";
+require($wphead_news_directory . '/wp-blog-header.php');
+
+?>
       <link rel="stylesheet" type="text/css" href="/theme/wordpress/style.css" />
 <?php
       wp_head();
